fb3e9de5cbba84ad0c3929f74d68037fb6d4d817
David Afonso Méndez <afonso27000@gmail.com>
2023-07-28 11:23:38 +0100

    including images through props

--- src/Personal-Digital-Assistant/PDAssistant.js
+++ src/Personal-Digital-Assistant/PDAssistant.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import ProfileCard from './ProfileCard'
+import AlexaImage from '../images/alexa.png'
+import CortanaImage from '../images/cortana.png'
+import SiriImage from '../images/siri.png'
 
 function PDAssistant() {
 
 
     return (
         <>
-            <div>
-                <div>Personal Digital Assistants</div>
-                <ProfileCard title='Alexa' handle='@alexa99' />
-                <ProfileCard title='Siri' handle='@Siri' />
-                <ProfileCard title='Cortana' handle='@Cortana' />
+            <div>Personal Digital Assistants</div>
+            <br/>
+            <div style={{display: 'flex', gap:'1rem'}}>
+                <ProfileCard img={AlexaImage} title='Alexa' handle='@alexa99' />
+                <ProfileCard img={CortanaImage} title='Siri' handle='@Siri' />
+                <ProfileCard img={SiriImage} title='Cortana' handle='@Cortana' />
             </div>
         </>
     )
